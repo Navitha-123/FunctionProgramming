674147e4481471d80ff7932a462f0668f66d0366
navitha <navithareddy112@gmail.com>
2022-08-28 18:49:49 +0530

    [navitha] 7.SwapNumber

--- FonctionProgramming/SwapNumber.cs
+++ FonctionProgramming/SwapNumber.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FonctionProgramming
 {
-    internal class Swap
+    internal class SwapNumber
     {
         public static void SwapTwoNumbers()
         {
